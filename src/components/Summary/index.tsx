ac7a784f3e016981130d88a8ade0b221c2999ef2
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-02-19 12:08:05 -0300

    calculando resumo de transacoes

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,53 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
-    return( 
-        <SummaryContainer>
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e"/>
-                </header>
+  const { transactions } = useContext(TransactionsContext);
 
-                <strong>R$ 17.400,00</strong>
-            </SummaryCard>
+  const sumary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
 
-            <SummaryCard>
-                <header>
-                    <span>Saídas</span>
-                    <ArrowCircleDown size={32} color="#f75a68"/>
-                </header>
+      return acc;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
 
-                <strong>R$ 17.400,00</strong>
-            </SummaryCard>
-            <SummaryCard variant="green">
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
 
-                <header>
-                    <span>Total</span>
-                    <CurrencyDollar size={32} color="#fff"/>
-                </header>
+        <strong>{sumary.income}</strong>
+      </SummaryCard>
 
-                <strong>R$ 17.400,00</strong>
-            </SummaryCard>
-        </SummaryContainer>
-    )
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>{sumary.outcome}</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>{sumary.total}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
 }
